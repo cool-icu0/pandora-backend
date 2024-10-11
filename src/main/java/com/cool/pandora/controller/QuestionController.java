d4ddde567a8f030b858cc1f5504997999f3fe02a
Cool <990299103@qq.com>
2024-10-11 21:09:35 +0800

    feat：“后端项目初始化，问题表单json字段修改”

--- src/main/java/com/cool/pandora/controller/QuestionController.java
+++ src/main/java/com/cool/pandora/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.cool.pandora.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cool.pandora.annotation.AuthCheck;
 import com.cool.pandora.common.BaseResponse;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 题目接口
@@ -216,6 +218,11 @@ public class QuestionController {
         // todo 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionEditRequest, question);
+        List<String> tags = questionEditRequest.getTags();
+        // 处理标签,将标签String列表转换为JSON字符串
+        if (tags!= null){
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
         // 数据校验
         questionService.validQuestion(question, false);
         User loginUser = userService.getLoginUser(request);
