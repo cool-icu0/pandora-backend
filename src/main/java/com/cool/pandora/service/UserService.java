fe3784555f57c9a570f523aff486f2e179f6c39f
Cool <990299103@qq.com>
2024-10-23 04:16:29 +0800

    feat：“接入redis的bitmap来控制刷题日历”

--- src/main/java/com/cool/pandora/service/UserService.java
+++ src/main/java/com/cool/pandora/service/UserService.java
@@ -6,7 +6,10 @@ import com.cool.pandora.model.dto.user.UserQueryRequest;
 import com.cool.pandora.model.entity.User;
 import com.cool.pandora.model.vo.LoginUserVO;
 import com.cool.pandora.model.vo.UserVO;
+
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
 
@@ -114,5 +117,24 @@ public interface UserService extends IService<User> {
      * @return
      */
     QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    /**
+     * 添加用户签到记录
+     *
+     * @param userId 用户 id
+     * @return 当前是否已签到成功
+     */
+    boolean addUserSignIn(long userId);
+
+    /**
+     * 获取用户某个年份的签到记录
+     *
+     * @param userId 用户 id
+     * @param year   年份（为空表示当前年份）
+     * @return 签到记录映射
+     */
+    List<Integer> getUserSignInRecord(long userId, Integer year);
+
+
 
 }
